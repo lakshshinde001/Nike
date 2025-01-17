53ad4a5890b9844c62e87787e34e76a03698fb14
lakshshinde001 <laxmanshinde3105@gmail.com>
2025-01-17 07:22:02 +0530

    Structures  is created in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import React from 'react'
 
-import './App.css'
-
-function App() {
+const App = () => {
   return (
-    <h1 className="container primary">
-      Exploreee!
-    </h1>
+    <main className='relative'>
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='padding '>
+        Popular Products
+      </section>
+      <section className='padding px- '>
+        SuperQualify
+      </section>
+      <section className='padding-x py-10 '>
+        Services
+      </section>
+      <section className='padding '>
+        Special Offers
+      </section>
+      <section className='bg-pale-blue padding '>
+        Customer Review
+      </section>
+      <section className='padding padding-x sm:py-32 py-16 w-full'>
+        Subscribe
+      </section>
+      <section className='padding bg-black padding-x padding-t pb-8'>
+        Footer
+      </section>
+    </main>
   )
 }
 
